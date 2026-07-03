ae0320e4eb64375699d06c04d537ebf2ad4cdf79
LynWM <murathimilynette@gmail.com>
2026-07-03 13:00:58 +0300

    feat: Installed new plugins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {BrowserRouter, Route, Router} from 'react-router-dom'
+
+import Navbar from './layouts/navbar'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Navbar>
+        <main className='min-h-screen'>
+
+        </main>
+      </Navbar>
+    </BrowserRouter>
   )
 }
