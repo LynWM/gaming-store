367eebd53a2357613941903dcb6f8f33b069ddc3
LynWM <murathimilynette@gmail.com>
2026-07-10 11:54:29 +0300

    feat:Added mock users js file

---
+++ client/src/data/mockUsers.js
@@ -0,0 +1,17 @@
+export const mockUsers = [
+  {
+    id: 1,
+    name: "Jane Doe",
+    email: "jane@test.com",
+    password: "password123", // plain text ok for mock/demo only
+    role: "customer",
+  },
+  {
+    id: 2,
+    name: "Admin User",
+    email: "admin@test.com",
+    password: "admin123",
+    role: "admin",
+  },
+  
+];
