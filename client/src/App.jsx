f5270e99c00cff98254035a892d13fdec3dc6c8f
LynWM <murathimilynette@gmail.com>
2026-07-10 10:53:08 +0300

    fix: Fixed UI components and flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import {BrowserRouter, Route, Router} from 'react-router-dom'
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom'
 
 import Navbar from './layouts/navbar'
 import Footer from './layouts/footer'
+import Homepage from './pages/customer/homepage'
+import Signup from './layouts/signup'
+import Login from './layouts/login'
 
 export default function App() {
   return (
@@ -10,6 +13,12 @@ export default function App() {
       <Navbar />
         <main className='min-h-screen'>
           {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Homepage /> } />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          
         </main>
       <Footer />
     </BrowserRouter>
